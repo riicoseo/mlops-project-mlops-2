8603758d6b59d84eb4b1ffafb5184c5a793d4140
yoonseo <riicoseo@naver.com>
2025-08-06 23:44:28 +0900

    feat: add airflow api

--- data_prepare/main.py
+++ data_prepare/main.py
@@ -1,5 +1,8 @@
+import os
 import pandas as pd
 from dotenv import load_dotenv
+
+from src.utils.utils import project_path
 
 # from preprocessing import TMDBPreProcessor
 from data_prepare.crawler import TMDBCrawler
@@ -10,7 +13,10 @@ def run_popular_movie_crawler():
     tmdb_crawler = TMDBCrawler()
     movie = tmdb_crawler.get_bulk_popular_movies(start_page=1, end_page=500)
     genre_name_to_id = tmdb_crawler.get_genre_name_to_id()
-    tmdb_crawler.save_movies_to_json_file(movie, genre_name_to_id, "./result", 'popular')
+    # tmdb_crawler.save_movies_to_json_file(movie, genre_name_to_id, "./result", 'popular')
+    popular_json_path = os.path.join(project_path(), "data_prepare", "result")  
+    tmdb_crawler.save_movies_to_json_file(movie, genre_name_to_id, popular_json_path, 'popular')
 
 if __name__ == "__main__":
     run_popular_movie_crawler()
+    print()
